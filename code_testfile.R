909965f47a3745c337acb61eb0ecd5faf30ebc3f
MWiehr <122365830+MWiehr@users.noreply.github.com>
2023-10-07 18:11:17 +0200

    wrote code so that the entire citation except the names work

--- code_testfile.R
+++ code_testfile.R
@@ -24,15 +24,25 @@ bib_file[i_true]
 #Possible Problems: There are different kinds of references needed
 
 #1 For i in i_true: Add a reference 
+# This is my example reference: Grady, J. S., Her, M., Moreno, G., Perez, C., & Yelinek, J. (2019). Emotions in storybooks: A comparison of storybooks that represent ethnic and racial groups in the United States. Psychology of Popular Media Culture, 8(3), 207–217. https://doi.org/10.1037/ppm0000185
+#Lastname, Firstletter., ... . (Year). title, pages. https://DOI
 references <- c()
-for (i in i_true) { 
+for (item in items) { 
+  nameallmembers <- "Names"
+  year <- result <- substr(gsub("[A-Za-z]", "", item$date[1]), 1, 4) #additionaly deleting the month and day from the date to fit the citation
+  title <- item$title[1]
+  pagenumbers <-paste( item$volume[1] , "," , item$pages[1])
+  doi <- item$DOI[1]
+  
+  reference <- paste(nameallmembers, "(",year,"). " , title , pagenumbers , ". https://" , doi)
   #reference <- iterate through allcreator in creators if firstname lastname pair not in our memberslist add creator[firstname] + ". " + creator[lastname] + ", " else name <- creator[firstname] + ". " + creator[lastname] + ", "  add "**" + name + "**" 
-  #then add the title to the str.  + "In:" + publicationTitle + "Doi:" + DOI
+
   
   references <- c(references, reference)
 }
-
-
+str1 <- "Hello "
+str2 <- "world"
+result <- paste(str1,str2)
 ################################################################################
 
 # Load the jsonlite library
@@ -45,7 +55,7 @@ items <- json_data$items
 #Going through all the papers we will look at all tags and check if they are m_true or g_true
 citation_keys <- c()
 i_true <- c()
-i = -1
+i = 0
 for (item in items) {
   tags <- item$tags
   i <- i + 1
